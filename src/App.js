51a7bea6987fa743eaeeaeaf0b328efdd33b96cd
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-04-17 02:10:04 +0600

    add banner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Component/About/About';
 import Blog from './Component/Blog/Blog';
@@ -12,15 +12,14 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      <BrowserRouter>
-        <Routes>
-          <Route to='/' element={<Home></Home>}></Route>
-          <Route to='/about' element={<About></About>}></Route>
-          <Route to='/blog' element={<Blog></Blog>}></Route>
-          <Route to='/class' element={<Class></Class>}></Route>
-          <Route to='/signin' element={<SignIn></SignIn>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/class' element={<Class></Class>}></Route>
+        <Route path='/signin' element={<SignIn></SignIn>}></Route>
+      </Routes>
+
       <Footer></Footer>
     </div>
   );
---
+++ src/Component/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Banner.css"
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className=""></div>
+            <div className="banner-text">
+                <h1>Yoga, release toxin out.</h1>
+                <p>Fins and book a great experience</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
