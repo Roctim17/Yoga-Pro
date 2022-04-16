6d8a857b5597dd1361a1fd7d95bd725bd10b0c3a
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-04-17 01:22:38 +0600

    install route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Component/About/About';
+import Blog from './Component/Blog/Blog';
+import Class from './Component/Class/Class';
+import Footer from './Component/Footer/Footer';
+import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
+import SignIn from './Component/SignIn/SignIn';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <BrowserRouter>
+        <Routes>
+          <Route to='/' element={<Home></Home>}></Route>
+          <Route to='/about' element={<About></About>}></Route>
+          <Route to='/blog' element={<Blog></Blog>}></Route>
+          <Route to='/class' element={<Class></Class>}></Route>
+          <Route to='/signin' element={<SignIn></SignIn>}></Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Component/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="logo">
+                <img src="/image/logo/yoga.png" alt="" />
+                <h3>Yoga Pro</h3>
+            </div>
+            <div className="menu">
+                <ul>
+                    <Link to="/" >Home</Link>
+                    <Link to="/about" >About</Link>
+                    <Link to="/blog" >Blog</Link>
+                    <Link to="/classes" >Classes</Link>
+                    <Link to="/signin" >Sign in</Link>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
